4252a961941788cbd45348602692dfd92da40adb
richard-jones <richard-jones@2bf6ea0f-123d-0410-b71a-f1a21eb24612>
2012-01-07 14:38:39 +0000

    separate namespace object into a separate spec file; we'll aim to move all constants relating to the spec here during the refactor
    git-svn-id: http://sword-app.svn.sourceforge.net/svnroot/sword-app/sss/branches/sss-2@423 2bf6ea0f-123d-0410-b71a-f1a21eb24612

--- sss/ingesters_disseminators.py
+++ sss/ingesters_disseminators.py
@@ -1,43 +1,10 @@
 from zipfile import ZipFile
 from lxml import etree
+from spec import Namespaces
 
 from sss_logging import logging
 ssslog = logging.getLogger(__name__)
 
-# FIXME: this is a duplicate of the one in core.  We need to sort out our
-# circular imports *urgh*
-class Namespaces(object):
-    """
-    This class encapsulates all the namespace declarations that we will need
-    """
-    def __init__(self):
-        # AtomPub namespace and lxml format
-        self.APP_NS = "http://www.w3.org/2007/app"
-        self.APP = "{%s}" % self.APP_NS
-
-        # Atom namespace and lxml format
-        self.ATOM_NS = "http://www.w3.org/2005/Atom"
-        self.ATOM = "{%s}" % self.ATOM_NS
-
-        # SWORD namespace and lxml format
-        self.SWORD_NS = "http://purl.org/net/sword/terms/"
-        self.SWORD = "{%s}" % self.SWORD_NS
-
-        # Dublin Core namespace and lxml format
-        self.DC_NS = "http://purl.org/dc/terms/"
-        self.DC = "{%s}" % self.DC_NS
-
-        # RDF namespace and lxml format
-        self.RDF_NS = "http://www.w3.org/1999/02/22-rdf-syntax-ns#"
-        self.RDF = "{%s}" % self.RDF_NS
-
-        # ORE namespace and lxml format
-        self.ORE_NS = "http://www.openarchives.org/ore/terms/"
-        self.ORE = "{%s}" % self.ORE_NS
-
-        # ORE ATOM
-        self.ORE_ATOM_NS = "http://www.openarchives.org/ore/atom/"
-        self.ORE_ATOM = "{%s}" % self.ORE_ATOM_NS
 
 class DisseminationPackager(object):
     def __init__(self, dao, uri_manager):
---
+++ sss/spec.py
@@ -0,0 +1,32 @@
+class Namespaces(object):
+    """
+    This class encapsulates all the namespace declarations that we will need
+    """
+    def __init__(self):
+        # AtomPub namespace and lxml format
+        self.APP_NS = "http://www.w3.org/2007/app"
+        self.APP = "{%s}" % self.APP_NS
+
+        # Atom namespace and lxml format
+        self.ATOM_NS = "http://www.w3.org/2005/Atom"
+        self.ATOM = "{%s}" % self.ATOM_NS
+
+        # SWORD namespace and lxml format
+        self.SWORD_NS = "http://purl.org/net/sword/terms/"
+        self.SWORD = "{%s}" % self.SWORD_NS
+
+        # Dublin Core namespace and lxml format
+        self.DC_NS = "http://purl.org/dc/terms/"
+        self.DC = "{%s}" % self.DC_NS
+
+        # RDF namespace and lxml format
+        self.RDF_NS = "http://www.w3.org/1999/02/22-rdf-syntax-ns#"
+        self.RDF = "{%s}" % self.RDF_NS
+
+        # ORE namespace and lxml format
+        self.ORE_NS = "http://www.openarchives.org/ore/terms/"
+        self.ORE = "{%s}" % self.ORE_NS
+
+        # ORE ATOM
+        self.ORE_ATOM_NS = "http://www.openarchives.org/ore/atom/"
+        self.ORE_ATOM = "{%s}" % self.ORE_ATOM_NS
