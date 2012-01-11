a25cf12180f82ff987d3145196b0ab0ece4ad417
richard-jones <richard-jones@2bf6ea0f-123d-0410-b71a-f1a21eb24612>
2012-01-11 20:54:58 +0000

    fix up module init file
    git-svn-id: http://sword-app.svn.sourceforge.net/svnroot/sword-app/sss/branches/sss-2@440 2bf6ea0f-123d-0410-b71a-f1a21eb24612

--- sss/__init__.py
+++ sss/__init__.py
@@ -1,3 +1,9 @@
+from info import __version__, __author__, __license__
+
+from core import Auth, AuthException, Authenticator, DeleteRequest, DeleteResponse, DepositRequest, DepositResponse, EntryDocument, SDCollection, SWORDRequest, ServiceDocument, Statement, SwordError, SwordServer
 from config import Configuration, SSS_CONFIG_FILE
+from spec import Errors, HttpHeaders
+
 import ingesters_disseminators
 import negotiator
+import repository
