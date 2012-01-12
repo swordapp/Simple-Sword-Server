f5c65c4a9db507cb92dce37fc71802a53882c113
richard-jones <richard-jones@2bf6ea0f-123d-0410-b71a-f1a21eb24612>
2012-01-12 19:16:33 +0000

    add contents of setup file
    git-svn-id: http://sword-app.svn.sourceforge.net/svnroot/sword-app/sss/branches/sss-2@441 2bf6ea0f-123d-0410-b71a-f1a21eb24612

---
+++ setup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+from setuptools import setup, find_packages
+
+setup(
+    name='SSS',
+    version='2.0',
+    description='Simple Sword Server',
+    author='Richard Jones',
+    author_email='rich.d.jones@gmail.com',
+    url='http://www.swordapp.org/',
+    packages=find_packages(exclude=['tests']),
+)
